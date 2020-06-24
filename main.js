8bacb2dbf9a1998560fa1495936f9a4c15fd48bc
rika0384 <sora6kuro0f9@yahoo.co.jp>
2020-06-25 01:11:31 +0900

    fix html

--- main.js
+++ main.js
@@ -5,22 +5,24 @@ const port = process.env.ENV_PORT || 3000,
 	app = express(),
 	db = require("./db/db");
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-var ejs = require('ejs');
-app.engine('ejs', ejs.renderFile);
+app.set("view engine", "ejs");
+app.use(express.static('public'));
 // jsonを扱えるようにする
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }))
 
 app.get('/', (req, res) => {
-    var msg = 'トップページです!';
-    res.render('index.ejs',
+    var contest = [{"contest_id":1,"contest_name":"ゆるふわ競技プログラミングオンサイト at FORCIA #2 ゴリラの挑戦状","contest_url":"yfkpo2","contest_date":"2019-09-13","writer":"prd_xxx, matsu7874"}];
+	res.render('./index.ejs',
         {
-            title:'Index',
-            content:msg,
+            contests:contest
 
         });
+});
+app.get('/add', (req, res) => {
+    res.render('./add.ejs');
 });
 
 app.get("/contest",async (req, res)=>{
